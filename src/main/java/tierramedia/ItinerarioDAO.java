e80fededfceb5dc50986d7bfe867b1b0c4e3c52e
Jorge Mora <telejam@hotmail.com>
2021-11-21 23:19:02 -0300

    Arreglo tabla mal en Itinerario

--- src/main/java/tierramedia/ItinerarioDAO.java
+++ src/main/java/tierramedia/ItinerarioDAO.java
@@ -9,15 +9,15 @@ public class ItinerarioDAO {
 	public int insert(int id_oferta, String tipo_oferta, int id_usuario,String nombre, double costo_total,double duracion_total) throws SQLException {
 		Connection connection = ConnectionProvider.getConnection();
 		String sql = "INSERT INTO itinerario  (id_oferta, tipo_oferta,  id_usuario, nombre, costo_total, duracion_total) VALUES (?, ?, ?, ?, ?, ? )";
-		PreparedStatement itinerario = connection.prepareStatement(sql);
-		itinerario.setInt(1, id_oferta);
-		itinerario.setString(2, tipo_oferta);
-		itinerario.setInt(3, id_usuario);
-		itinerario.setString(4, nombre);
-		itinerario.setDouble(5, costo_total);
-		itinerario.setDouble(6, duracion_total);
+		PreparedStatement itinerarios = connection.prepareStatement(sql);
+		itinerarios.setInt(1, id_oferta);
+		itinerarios.setString(2, tipo_oferta);
+		itinerarios.setInt(3, id_usuario);
+		itinerarios.setString(4, nombre);
+		itinerarios.setDouble(5, costo_total);
+		itinerarios.setDouble(6, duracion_total);
 
-		int rows = itinerario.executeUpdate();
+		int rows = itinerarios.executeUpdate();
 		
 		return rows;
 	}
