d83472ff5d84a67e49a62a65d63201888cdb3a11
Jorge Mora <telejam@hotmail.com>
2021-11-21 22:24:48 -0300

    throw en portal

--- src/main/java/tierramedia/PortalATierraMedia.java
+++ src/main/java/tierramedia/PortalATierraMedia.java
@@ -1,7 +1,9 @@
 package tierramedia;
 
+import java.sql.SQLException;
+
 public class PortalATierraMedia {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		System.out.println("Bienvenido a la Tierra Media");
 		System.out.println("\n-------------------------------------------------------------------\n");
 		
